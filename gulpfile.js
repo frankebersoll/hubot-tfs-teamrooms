23a7291393384974b458c68464563053f28cdd76
Frank Ebersoll <frank.ebersoll@streamteam.de>
2016-02-02 07:37:15 +0100

    Source maps

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,33 @@ var coffee = require('gulp-coffee');
 var sourcemaps = require('gulp-sourcemaps');
 var path = require('path');
 
+var sourceRoot = (dest, file) => {
+  var output = path.join(dest, file.relative);
+  var dir = path.dirname(output);
+  return path.relative(dir, file.base);
+};
+
 gulp.task('hubot', function() {
   gulp.src('./node_modules/hubot/**/*.coffee')
     .pipe(sourcemaps.init())
     .pipe(coffee({ bare: true })
     .on('error', gutil.log))
-    .pipe(sourcemaps.write('.', {sourceRoot: path.resolve(__dirname + '/node_modules/hubot/')}))
+    .pipe(sourcemaps.write('.', {
+      includeContent: false,
+      sourceRoot: sourceRoot.bind(null, './dest/hubot/')
+    }))
     .pipe(gulp.dest('./dest/hubot/'));
 });
 
 gulp.task('coffee', function() {
-  gulp.src('./src/*.coffee')
+  gulp.src('./src/**/*.coffee')
     .pipe(sourcemaps.init())
     .pipe(coffee({ bare: true })
     .on('error', gutil.log))
-    .pipe(sourcemaps.write('.', {sourceRoot: path.resolve(__dirname + '/src/')}))
+    .pipe(sourcemaps.write('.', {
+      includeContent: false,
+      sourceRoot: sourceRoot.bind(null, './dest/')
+    }))
     .pipe(gulp.dest('./dest/'));
 });
 
